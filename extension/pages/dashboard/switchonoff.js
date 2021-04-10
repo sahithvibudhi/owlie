2598ea8c551d3c4ca967e9c546cebb9db90920d1
SathvikPalley <sathvik.palley@gmail.com>
2021-04-10 00:55:44 -0500

    add switch status

---
+++ extension/pages/dashboard/switchonoff.js
@@ -0,0 +1,24 @@
+function onpageload(){
+
+document.getElementById("ytcheck").addEventListener("click", statuscheck);
+document.getElementById("spcheck").addEventListener("click", statuscheck);
+document.getElementById("nfcheck").addEventListener("click", statuscheck);
+
+function statuscheck() {
+
+    let statusdict = {
+
+        "youtube": document.getElementById("youtube-cb").checked,
+        
+        "netflix": document.getElementById("netflix-cb").checked,
+
+        "spotify": document.getElementById("spotify-cb").checked,
+    }
+
+    console.log(statusdict);
+    
+ }
+
+ }
+
+ window.onload = onpageload;
