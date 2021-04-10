ec283613d25d65c19c9c01129f0d59da82e7df2f
sahithvibudhi <v.sahithkumar@gmail.com>
2021-04-10 17:21:44 -0500

    track location via injector

--- extension/background-workers/injector.js
+++ extension/background-workers/injector.js
@@ -1,9 +1,40 @@
+const ENDPOINT = 'http://localhost:3000';
+
+function ajax({
+    endpoint,
+    method,
+    data,
+    success
+}) {
+    console.log('saving ');
+    var myHeaders = new Headers();
+    myHeaders.append("token", window.token);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(data);
+
+    var requestOptions = {
+        method,
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    console.log(`${ENDPOINT}${endpoint}`, requestOptions);
+    fetch(`${ENDPOINT}${endpoint}`, requestOptions)
+        .then(response => response.text())
+        .then(result => success(result))
+        .catch(error => console.log('error', error));
+}
+
 var urlFound = '';
-(function ()
- {
-    setInterval(checkNewURL, 2000);
- })
- ();
+
+chrome.storage.local.get(['token'], function(result) {
+    if (result && result.token) {
+        window.token = result.token;
+        setInterval(checkNewURL, 2000);
+    }
+});
+
 function checkNewURL() {
     if (window.location.href != urlFound) {
         urlFound = window.location.href;
@@ -13,4 +44,14 @@ function checkNewURL() {
  function onNewURLDiscover() {
     //alert("hello")
     console.log("found new path "+urlFound)
+    ajax({
+        endpoint: '/activity',
+        method: 'POST',
+        data: {
+            location: urlFound
+        },
+        success: function(data) {
+            console.log(data);
+        }
+    });
  }
